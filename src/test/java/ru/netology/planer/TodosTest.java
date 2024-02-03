41ab8e1fe2bbe27c1c6f466e7cdc5c8d312c1d6b
MarinaTret <jiharka88@mail.ru>
2024-02-03 14:11:53 +0300

    modification

--- src/test/java/ru/netology/planer/TodosTest.java
+++ src/test/java/ru/netology/planer/TodosTest.java
@@ -2,7 +2,7 @@ package ru.netology.planer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class TodosTest {
-    @Test
+    @Test //находит все задачи
     public void shouldAddThreeTasksOfDifferentType() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
@@ -27,7 +27,82 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
+    @Test //находится несколько задач - SimpleTask и Epic
+    public void shouldSimpleTaskAndEpic() {
+        SimpleTask simpleTask = new SimpleTask(5, "Молоко");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic};
+        Task[] actual = todos.search("Молоко");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test //находится несколько задач - SimpleTask и Meeting
+    public void shouldSimpleTaskAndMeeting() {
+        SimpleTask simpleTask = new SimpleTask(5, "Выкатка 3й версии приложения");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("Выкатка 3й версии приложения");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test //находится несколько задач - Epic и Meeting
+    public void shouldEpicAndMeeting() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Молоко",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {epic, meeting};
+        Task[] actual = todos.search("Молоко");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test //находится одна задача SimpleTask
     public void shouldSimpleTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
@@ -53,7 +128,7 @@ public class TodosTest {
     }
 
 
-    @Test
+    @Test //находится одна задача Epic
     public void SearchEpic() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
@@ -78,7 +153,7 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
+    @Test //находится одна задача Meeting
     public void SearchMeeting() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
@@ -103,7 +178,7 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
+    @Test //находит 0 задач, ни одна задача не подходит
     public void NotSearch() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
@@ -127,5 +202,4 @@ public class TodosTest {
         Task[] actual = todos.search("Курица");
         Assertions.assertArrayEquals(expected, actual);
     }
-
 }
